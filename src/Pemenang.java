472cf077b791bbd33438c66dd83c8a2f2f312c67
isma <ismajannah999@gmail.com>
2016-03-09 09:03:59 +0700

    pake split java

--- src/Pemenang.java
+++ src/Pemenang.java
@@ -6,14 +6,20 @@ public class Pemenang {
     int jumlah_waktu[];
     int jumlahPutaran[];
         
-    public int cari_pemenang(String nim[],String nama_mahasiswa[], Lapangan b,int i, int jumlah_waktu[], int jumlahPutaran[]){
+    public void cari_pemenang(String nim[],String nama_mahasiswa[], Lapangan b,int i, int jumlah_waktu[], int jumlahPutaran[]){
         Mahasiswa m[] = new Mahasiswa[4];
-        for(i = 0; i < 4 ; i++){
-            m[i] = new Mahasiswa(nim, nama_mahasiswa, jumlahPutaran, jumlah_waktu);
-           return m[i].hitungJarak(b, i)/jumlah_waktu[i];
+        
+        
+            
             
         }
-   
+            
+ 
+    
+    }
+    
+
+
         
     
 
--- src/mainLari.java
+++ src/mainLari.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class mainLari {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);  
-        Scanner inn = new Scanner(System.in);
-        Scanner innn = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+       
 
+     
         int jumlah = in.nextInt();
         Mahasiswa m[] = new Mahasiswa[jumlah];
 
@@ -18,31 +18,43 @@ public class mainLari {
 
         for (int i = 0; i < m.length; i++) {
 
-
+            
+            
             String nama_mahasiswa[] = new String[m.length];
             String nim[] = new String[m.length];
-
-            System.out.println("nim : ");
-            nim[i] = innn.nextLine();
-            System.out.println("nama : ");
-            nama_mahasiswa[i] = innn.nextLine();
-
-         
-
             int jumlahPutaran[] = new int[m.length];
             int jumlah_waktu[] = new int[m.length];
 
-            System.out.println("putaran : ");
-            jumlahPutaran[i] = in.nextInt();
-            System.out.println("waktu : ");
-            jumlah_waktu[i] = inn.nextInt();
+            String a = in.next();
+            String b = in.next();
+            String c = in.next();
+            String d = in.next();
+           
+            
+            
+            String Tulisan = a +","+ b +","+ c +","+ d;
+            String[] pecah = Tulisan.split(",");
+            
+            int c1 = Integer.parseInt(pecah[4]);
+            int d1 = Integer.parseInt(pecah[6]);
+            
+            nim[i] = pecah[0];
+
+            nama_mahasiswa[i] = pecah[2];
+
+            jumlahPutaran[i] = c1;
+
+            jumlah_waktu[i] = d1;
+            
 
             m[i] = new Mahasiswa(nim, nama_mahasiswa, jumlahPutaran, jumlah_waktu);
 
         }
+ 
+        System.out.println("OUTPUT");
 
         for (int i = 0; i < m.length; i++) {
-            
+       
             m[i].Tampil(lap, i);
         }
 
